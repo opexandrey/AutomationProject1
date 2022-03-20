3f990ae0a03a0d58cf84f06b662a339bd54c8558
Andrey O <opexandrey@gmail.com>
2022-03-19 22:39:48 -0400

    1st commit

---
+++ src/Project1.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Project1 {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","C:\\__DuoTech\\SoftWare\\Automation\\Selenium\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+
+
+
+
+    }
+}
